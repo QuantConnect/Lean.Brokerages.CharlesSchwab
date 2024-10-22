b4706f2203a7c5b22b393c742479dd937c9ec6de
Romazes <romazes20@gmail.com>
2024-10-22 15:05:44 +0300

    refactor: use partial class and split by different files logic

---
+++ QuantConnect.CharlesSchwabBrokerage/CharlesSchwabBrokerage.DataQueueUniverseProvider.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using QuantConnect.Interfaces;
+using System.Collections.Generic;
+
+namespace QuantConnect.Brokerages.CharlesSchwab;
+
+public partial class CharlesSchwabBrokerage : IDataQueueUniverseProvider
+{
+    /// <summary>
+    /// Method returns a collection of Symbols that are available at the data source.
+    /// </summary>
+    /// <param name="symbol">Symbol to lookup</param>
+    /// <param name="includeExpired">Include expired contracts</param>
+    /// <param name="securityCurrency">Expected security currency(if any)</param>
+    /// <returns>Enumerable of Symbols, that are associated with the provided Symbol</returns>
+    public IEnumerable<Symbol> LookupSymbols(Symbol symbol, bool includeExpired, string securityCurrency = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Returns whether selection can take place or not.
+    /// </summary>
+    /// <remarks>This is useful to avoid a selection taking place during invalid times, for example IB reset times or when not connected,
+    /// because if allowed selection would fail since IB isn't running and would kill the algorithm</remarks>
+    /// <returns>True if selection can take place</returns>
+    public bool CanPerformSelection()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ QuantConnect.CharlesSchwabBrokerage/CharlesSchwabBrokerage.HistoryProvider.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using QuantConnect.Data;
+using System.Collections.Generic;
+
+namespace QuantConnect.Brokerages.CharlesSchwab;
+
+public partial class CharlesSchwabBrokerage
+{
+    /// <summary>
+    /// Gets the history for the requested symbols
+    /// <see cref="IBrokerage.GetHistory(Data.HistoryRequest)"/>
+    /// </summary>
+    /// <param name="request">The historical data request</param>
+    /// <returns>An enumerable of bars covering the span specified in the request</returns>
+    public override IEnumerable<BaseData> GetHistory(HistoryRequest request)
+    {
+        if (!CanSubscribe(request.Symbol))
+        {
+            return null; // Should consistently return null instead of an empty enumerable
+        }
+
+        throw new NotImplementedException();
+    }
+}
