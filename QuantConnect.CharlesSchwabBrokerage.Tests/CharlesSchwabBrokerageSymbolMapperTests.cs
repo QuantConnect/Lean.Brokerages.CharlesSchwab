e1a5623eb88310078fc2e63ed648e28afe41261e
Romazes <romazes20@gmail.com>
2024-10-29 13:47:41 +0200

    feat: GetBrokerageSymbol test:feat: GetBrokerageSymbol

--- QuantConnect.CharlesSchwabBrokerage.Tests/CharlesSchwabBrokerageSymbolMapperTests.cs
+++ QuantConnect.CharlesSchwabBrokerage.Tests/CharlesSchwabBrokerageSymbolMapperTests.cs
@@ -13,23 +13,85 @@
  * limitations under the License.
 */
 
+using System;
 using NUnit.Framework;
+using QuantConnect.Tests;
+using System.Collections.Generic;
 
-namespace QuantConnect.Brokerages.CharlesSchwab.Tests
+namespace QuantConnect.Brokerages.CharlesSchwab.Tests;
+
+[TestFixture]
+public class CharlesSchwabBrokerageSymbolMapperTests
 {
-    [TestFixture, Ignore("Not implemented")]
-    public class CharlesSchwabBrokerageSymbolMapperTests
+    /// <summary>
+    /// Provides the mapping between Lean symbols and brokerage specific symbols.
+    /// </summary>
+    private CharlesSchwabBrokerageSymbolMapper _symbolMapper;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
     {
-        [Test]
-        public void ReturnsCorrectLeanSymbol()
+        _symbolMapper = new CharlesSchwabBrokerageSymbolMapper();
+    }
+
+    [Test]
+    public void ReturnsCorrectLeanSymbol()
+    {
+
+    }
+
+    private static IEnumerable<TestCaseData> LeanSymbolTestCases
+    {
+        get
         {
-
+            TestGlobals.Initialize();
+            // TSLA - Equity
+            var underlying = Symbol.Create("TSLA", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(underlying, "TSLA");
+            yield return new TestCaseData(Symbol.CreateOption(underlying, Market.USA, OptionStyle.American, OptionRight.Call, 252.5m, new DateTime(2024, 11, 1)), "TSLA  241101C00252500");
+            yield return new TestCaseData(Symbol.CreateOption(underlying, Market.USA, OptionStyle.American, OptionRight.Call, 260m, new DateTime(2024, 12, 06)), "TSLA  241206C00260000");
+            yield return new TestCaseData(Symbol.CreateOption(underlying, Market.USA, OptionStyle.American, OptionRight.Put, 252.5m, new DateTime(2024, 11, 01)), "TSLA  241101P00252500");
+            // SPX - Index
+            var SPX = Symbol.Create("SPX", SecurityType.Index, Market.USA);
+            yield return new TestCaseData(SPX, "$SPX");
+            yield return new TestCaseData(Symbol.CreateOption(SPX, Market.USA, SecurityType.IndexOption.DefaultOptionStyle(), OptionRight.Call,
+                5805m, new DateTime(2024, 11, 15)), "SPX   241115C05805000");
+            // SPXW - Index
+            var SPXW = Symbol.Create("SPXW", SecurityType.Index, Market.USA);
+            yield return new TestCaseData(SPXW, "$SPXW");
+            yield return new TestCaseData(Symbol.CreateOption(SPXW, Market.USA, SecurityType.IndexOption.DefaultOptionStyle(), OptionRight.Call,
+                5925, new DateTime(2025, 09, 30)), "SPXW  250930C05925000");
+            // F - Equity
+            var F = Symbol.Create("F", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(F, "F");
+            yield return new TestCaseData(Symbol.CreateOption(F, Market.USA, SecurityType.Option.DefaultOptionStyle(), OptionRight.Put, 6.5m, new DateTime(2024, 11, 01)), "F     241101P00006500");
+            // DJT - Equity
+            var DJT = Symbol.Create("DJT", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(DJT, "DJT");
+            yield return new TestCaseData(Symbol.CreateOption(DJT, Market.USA, SecurityType.Option.DefaultOptionStyle(), OptionRight.Call, 1m, new DateTime(2024, 11, 01)), "DJT   241101C00001000");
+            // IRBT - Equity
+            var IRBT = Symbol.Create("IRBT", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(IRBT, "IRBT");
+            yield return new TestCaseData(Symbol.CreateOption(IRBT, Market.USA, SecurityType.Option.DefaultOptionStyle(), OptionRight.Call, 1m, new DateTime(2024, 11, 01)), "IRBT  241101C00001000");
+            // GOOGL - Equity
+            var GOOGL = Symbol.Create("GOOGL", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(GOOGL, "GOOGL");
+            yield return new TestCaseData(Symbol.CreateOption(GOOGL, Market.USA, SecurityType.Option.DefaultOptionStyle(), OptionRight.Put, 180m, new DateTime(2024, 12, 06)), "GOOGL 241206P00180000");
+            // DJI - Index
+            yield return new TestCaseData(Symbol.Create("DJI", SecurityType.Index, Market.USA), "$DJI");
+            // ES - Future
+            yield return new TestCaseData(Symbol.CreateFuture("ES", Market.USA, new DateTime(2024, 12, 10)), "/ESZ24");
+            yield return new TestCaseData(Symbol.CreateFuture("ES", Market.USA, new DateTime(2024, 5, 10)), "/ESK24");
         }
+    }
 
-        [Test]
-        public void ReturnsCorrectBrokerageSymbol()
-        {
+    [Test, TestCaseSource(nameof(LeanSymbolTestCases))]
+    public void ReturnsCorrectBrokerageSymbol(Symbol symbol, string expectedBrokerageSymbol)
+    {
+        var brokerageSymbol = _symbolMapper.GetBrokerageSymbol(symbol);
 
-        }
+        Assert.IsNotNull(brokerageSymbol);
+        Assert.IsNotEmpty(brokerageSymbol);
+        Assert.That(brokerageSymbol, Is.EqualTo(expectedBrokerageSymbol));
     }
 }
--- QuantConnect.CharlesSchwabBrokerage/CharlesSchwabBrokerage.SymbolMapper.cs
+++ QuantConnect.CharlesSchwabBrokerage/CharlesSchwabBrokerage.SymbolMapper.cs
@@ -14,6 +14,7 @@
 */
 
 using System;
+using System.Collections.Generic;
 
 namespace QuantConnect.Brokerages.CharlesSchwab;
 
@@ -22,18 +23,58 @@ namespace QuantConnect.Brokerages.CharlesSchwab;
 /// </summary>
 public class CharlesSchwabBrokerageSymbolMapper : ISymbolMapper
 {
+    /// <summary>
+    /// Represents a set of supported security types.
+    /// </summary>
+    /// <remarks>
+    /// This HashSet contains the supported security types that are allowed within the system.
+    /// </remarks>
+    public readonly HashSet<SecurityType> SupportedSecurityType = new()
+    {
+        SecurityType.Index,
+        SecurityType.Equity,
+        SecurityType.Future,
+        SecurityType.Option,
+        SecurityType.IndexOption,
+    };
+
+    /// <summary>
+    /// Converts a Lean symbol instance to a brokerage symbol
+    /// </summary>
+    /// <param name="symbol">A Lean symbol instance</param>
+    /// <returns> The brokerage symbol</returns>
+    /// <exception cref="NotImplementedException">The lean security type is not implemented.</exception>
     public string GetBrokerageSymbol(Symbol symbol)
     {
         switch (symbol.SecurityType)
         {
             case SecurityType.Equity:
                 return symbol.Value;
+            case SecurityType.Future:
+                return GenerateBrokerageFuture(symbol);
+            case SecurityType.Index:
+                return GenerateBrokerageIndex(symbol);
+            case SecurityType.Option:
+                return GenerateBrokerageOption(symbol);
+            case SecurityType.IndexOption:
+                return GenerateBrokerageOption(symbol);
             default:
                 throw new NotImplementedException($"{nameof(CharlesSchwabBrokerageSymbolMapper)}.{nameof(GetBrokerageSymbol)}: " +
                     $"The security type '{symbol.SecurityType}' is not supported.");
         }
     }
 
+    /// <summary>
+    /// Converts a brokerage symbol to a Lean symbol instance
+    /// </summary>
+    /// <param name="brokerageSymbol">The brokerage symbol</param>
+    /// <param name="securityType">The security type</param>
+    /// <param name="market">The market</param>
+    /// <param name="expirationDate">Expiration date of the security(if applicable)</param>
+    /// <param name="strike">The strike of the security (if applicable)</param>
+    /// <param name="optionRight">The option right of the security (if applicable)</param>
+    /// <returns>A new Lean Symbol instance</returns>
+    /// <exception cref="NotImplementedException">The security type is not implemented or not supported.</exception>
     public Symbol GetLeanSymbol(string brokerageSymbol, SecurityType securityType, string market, DateTime expirationDate = default, decimal strike = 0, OptionRight optionRight = OptionRight.Call)
     {
         switch (securityType)
@@ -44,5 +85,39 @@ public class CharlesSchwabBrokerageSymbolMapper : ISymbolMapper
                 throw new NotImplementedException($"{nameof(CharlesSchwabBrokerageSymbolMapper)}.{nameof(GetLeanSymbol)}: " +
                     $"The security type '{securityType}' with brokerage symbol '{brokerageSymbol}' is not supported.");
         }
+    }
+
+    /// <summary>
+    /// Generates a brokerage option string based on the Lean symbol.
+    /// </summary>
+    /// <param name="symbol">The symbol object containing information about the option.</param>
+    /// <returns>A string representing the brokerage option.</returns>
+    /// <example>{AAPL 240510C167.5}</example>
+    private string GenerateBrokerageOption(Symbol symbol)
+    {
+        var strike = symbol.ID.StrikePrice.ToStringInvariant("00000.000").Replace(".", "");
+        return $"{symbol.Underlying.Value,-6}{symbol.ID.Date:yyMMdd}{symbol.ID.OptionRight.ToString()[0]}{strike}";
+    }
+
+    /// <summary>
+    /// Generates a brokerage future string based on the provided symbol.
+    /// </summary>
+    /// <param name="symbol">The symbol object containing information about the future.</param>
+    /// <returns>A string representing the brokerage future.</returns>
+    /// <example>{/ESZ24}</example>
+    private string GenerateBrokerageFuture(Symbol symbol)
+    {
+        return $"/{symbol.ID.Symbol}{SymbolRepresentation.FuturesMonthLookup[symbol.ID.Date.Month]}{symbol.ID.Date:yy}";
+    }
+
+    /// <summary>
+    /// Generates a brokerage index string based on the provided symbol.
+    /// </summary>
+    /// <param name="symbol">The symbol object containing information about the index.</param>
+    /// <returns>A string representing the brokerage index.</returns>
+    /// <example>{$SPX}</example>
+    private string GenerateBrokerageIndex(Symbol symbol)
+    {
+        return "$" + symbol.Value;
     }
 }
